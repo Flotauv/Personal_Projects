c80efdb3d25a2a387c6dad8d44fb57cbd98ac4bf
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-20 19:26:48 +0000

    set up config pages

---
+++ Iris_prediction/pages/Introduction.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+
+# --- Set Page Config 
+
+st.set_page_config(
+    page_title="Introduction",
+    layout="centered",
+)
--- Iris_prediction/streamlit.py
+++ Iris_prediction/streamlit.py
@@ -2,5 +2,32 @@
 import streamlit as st
 
 
+#Page Setup
+
+about_page_intro = st.Page(
+    page='pages/Introduction.py',
+    title='Introduction',
+    #icon = ":material/account_circle:",
+    default=True
+
+)
+
+project_iris_page_1 = st.Page(
+    page='pages/Data.py',
+    title="Découverte des données",
+    #icon=':material/bar_chart:',
+    
+)
+
+# --- Navigation SETUP ---
+
+pg = st.navigation(pages=[about_page_intro,project_iris_page_1])
+
+
+# --- Navigation RUN 
+
+pg.run()
+
+
 #def __main__():
     #return 

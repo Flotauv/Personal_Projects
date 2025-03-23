a50fc816b02a933c8681ab440c59410a728b4846
Florent Saunier <florent.saunier13@gmail.com>
2025-03-23 17:04:37 +0100

    second print

--- Learning_C/test.c
+++ Learning_C/test.c
@@ -2,3 +2,18 @@
 // debug et release sont les modes de compilation
 // debug il avance ligne pqr ligne
 // release il enlève des variables , il essaye de l'optimiser 
+
+// header pour utiliser les flux , et c'est un package qu on importe 
+#include <stdio.h> 
+
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = a + b;
+    printf("%d",c);
+    //std::cout << "Hello world "<< std::endl; 
+    //std::cin.ignore();
+    return 0;
+}
+

e4f46b038b06a437e3d30b00d4ccbf654e9357e0
Florent Saunier <florent.saunier13@gmail.com>
2025-03-23 16:48:52 +0100

    first steps

---
+++ Learning_C/test.c
@@ -0,0 +1,4 @@
+// le compilateur permet de traduire le code pour notre machine 
+// debug et release sont les modes de compilation
+// debug il avance ligne pqr ligne
+// release il enlève des variables , il essaye de l'optimiser 

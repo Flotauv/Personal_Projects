0bc5d636255405f1fbca0e0b9ff5956b1b0f9a51
Florent Saunier <florent.saunier13@gmail.com>
2025-04-12 17:50:24 +0200

    Pas à pas C

--- Learning_C/test.c
+++ Learning_C/test.c
@@ -5,6 +5,13 @@
 
 // header pour utiliser les flux , et c'est un package qu on importe 
 #include <stdio.h> 
+/*
+If I want to put comments on multiple lines, I can use this method
+I can also use // to comment on a single line
+I can also use #define to define a constant
+I can also use #include to include a library
+*/
+
 
 
 int fct_test(int a,int b){
@@ -14,15 +21,22 @@ int fct_test(int a,int b){
 int main() {
     double number1;
     char alphabet;
-    printf("Entrez des valeurs: ");
+
+    printf("Entrez des valeurs (Un double et un caractère): \n");
+    //scanf("%lf %c",&number1,&alphabet);
+    //printf("Le nombre entré est :%lf",number1);
+    //printf("\n La lettre entrée est :%c",alphabet);
+
+    double variable = 12;
+    double x = variable+1;
+    printf("%lf \n",--x);
+
+    int valeur1 = 5.5;
+    int valeur2 = 6;
+
+    printf("%d",valeur1);
+
     
-    scanf("%lf %c",&number1,&alphabet);
-
-
-
-    printf("Le nombre entré est :%lf",number1);
-    printf("\n La lettre entrée est :%c",alphabet);
-
 
     //printf(fct_test(5,3));   
     return 0;
